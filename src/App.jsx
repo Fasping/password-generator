90f29f61ed8044a553c035a68274939f4f4814e0
Fernando Cases <nandocasesgarcia@gmail.com>
2023-02-13 15:30:31 +0100

    styled: add styled comp and form basics

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,56 @@
 import "./App.css";
+import styled from "styled-components";
+
+const Titulo = styled.h1`
+  width: 100%;
+  margin-bottom: 80px;
+  vertical-align: top;
+`;
+
+const Fila = styled.div`
+  margin-bottom: 40px;
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  gap: 10px;
+`;
+
+const Button = styled.button`
+  width: 100%;
+  background-color: #684eff;
+  color: #fff;
+  display: inline-flex;
+  align-items: center;
+  height: 40px;
+  vertical-align: top;
+  font-size: 18px;
+  text-align: center;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+  transition: all 0.3s ease;
+
+  &:hover {
+    background: #866ffd;
+  }
+`;
 
 const App = () => {
   return (
     <div className="contenedor">
-      <p> App component !!!</p>
+      <Titulo>
+        <h1>Generator Password</h1>
+      </Titulo>
+
+      <form>
+        <Fila>
+          <label>Number caracters:</label>
+          <div>
+            <Button>-</Button>
+            <span>0</span>
+            <Button>+</Button>
+          </div>
+        </Fila>
+      </form>
     </div>
   );
 };
